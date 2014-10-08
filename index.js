e85ec008cf4c399013a07e91b2fba5cfc70795c8
xeodou <xeodou@gmail.com>
2014-10-08 18:35:51 +0800

    Update route

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ var debug = require('debug')('redirectly:index');
 
 var http = require('http'),
     koa = require('koa'),
-    app = koa();
+    app = koa(),
+    mount = require('koa-mount'),
+    Router = require('koa-router');
 
 app.use(function *(next) {
     this.body = 'Hello Heroku';
     yield next;
 });
+
+var url = function (){
+
+    var url = new Router()
+    url.get('/', function *(next) {
+        if(this.query && this.query.redirect)
+            this.redirect(this.query.redirect)
+        else
+            this.body = {}
+        yield next;
+    })
+    return url.middleware();
+}
+
+app.use(mount('/url', url()))
 
 http.createServer(app.callback()).listen(process.env.PORT, function() {
     debug('listening port %s', process.env.PORT)
